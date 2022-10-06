208a9a66473785731c685fb455d54c0fd15f822d
tuanlinhtony <mrpi3990@gmail.com>
2022-10-06 22:58:29 +0700

    create profile api

---
+++ profiles_api/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+
+class UpdateOwnerProfile(permissions.BasePermission):
+    """Allow user to edit their own profile"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check user is trying to edit their own profile"""
